4ab4591610f463e32a06f1463bd39217b581736d
RohanMali2003 <rohanmali132003@gmail.com>
2025-01-02 22:07:29 +0530

    Updated appointments

--- mocks/generator.js
+++ mocks/generator.js
@@ -28,46 +28,36 @@ const createPatients = () => {
 const createProcedures = () => {
   return Array.from({ length: 100 }, () => ({
     id: faker.number.int({ min: 1, max: 9999 }),
-    procedureId: faker.number.int({ min: 1, max: 9999 }),
-    procedureCashierName: faker.person.fullName(),
-    cashPayment: faker.number.int({ min: 1, max: 9999 }),
-    clinicName: faker.company.name(),
-    discount: faker.number.int({ min: 10, max: 30 }),
-    finalAmount: faker.number.int({ min: 1, max: 9999 }),
-    procedureDate: faker.date.recent(),
-    onlinePayment: faker.number.int({ min: 1, max: 9999 }),
-    procedureDetails: faker.lorem.sentence(),
-    procedureType: faker.lorem.sentence(),
-    procedureTime: faker.date.timestamp,
-    patientReports: faker.lorem.sentence(),
-    totalAmount: faker.number.int({ min: 1, max: 9999 }),
-    patientName: faker.person.fullName(),
+    procedureName: faker.lorem.words(),
+    procedureDescription: faker.lorem.sentence(),
+    procedureCost: faker.number.float({ min: 100, max: 10000, precision: 0.01 }),
+    timestamp: faker.date.recent(),
   }));
 };
 
 const createUsers = () => {
-  return Array.from({ length: 10 }, () => ({
+  return Array.from({ length: 100 }, () => ({
     id: faker.number.int({ min: 1, max: 9999 }),
-    userId: faker.number.int({ min: 1, max: 9999 }),
-    username: faker.internet.username(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
     password: faker.internet.password(),
-    role: faker.animal.type(),
-    mobileNumber: faker.phone.number(),
+    role: faker.helpers.arrayElement(['admin', 'user']),
+    timestamp: faker.date.recent(),
   }));
 };
 
 const createMedicines = () => {
   return Array.from({ length: 100 }, () => ({
-    medicineId: faker.number.int({ min: 1, max: 9999 }),
-    medicineName: faker.person.fullName(),
-    medicinePack: faker.number.int({ min: 1, max: 9999 }),
-    medicineType: faker.lorem.sentence(),
-    medicinePrice: faker.number.int({ min: 1, max: 9999 }),
+    id: faker.number.int({ min: 1, max: 9999 }),
+    medicineName: faker.lorem.words(),
+    medicineDescription: faker.lorem.sentence(),
+    medicineCost: faker.number.float({ min: 10, max: 500, precision: 0.01 }),
+    timestamp: faker.date.recent(),
   }));
 };
 
 const createAppointments = () => {
-  return Array.from({ length: 100 }, () => ({
+  const appointments = Array.from({ length: 100 }, () => ({
     id: faker.number.int({ min: 1, max: 9999 }),
     appointmentId: faker.number.int({ min: 1, max: 9999 }),
     firstName: faker.person.firstName(),
@@ -78,8 +68,10 @@ const createAppointments = () => {
     appointmentDate: faker.date.recent(),
     patientMobile1: faker.phone.number(),
     cashierName: faker.person.firstName(),
-    timestamp: faker.date.timestamp,
+    timestamp: faker.date.recent(),
   }));
+  console.log(appointments); // Debugging statement to check generated data
+  return appointments;
 };
 
 const procedures = createProcedures();
@@ -93,7 +85,6 @@ const jsonData = JSON.stringify({ patients, procedures, users, medicines, appoin
 if (fs.existsSync(`${dir}/db.json`)) {
   console.log('db.json already exists!');
 } else {
-  fs.writeFile(`${dir}/db.json`, jsonData, 'utf8', (err) => {
-    console.log(err || 'db.json generated successfully!');
-  });
+  fs.writeFileSync(`${dir}/db.json`, jsonData);
+  console.log('db.json has been created with the following data:', jsonData); // Debugging statement to check written data
 }
